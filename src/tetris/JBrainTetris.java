c1dff1868b235a641eb036786060c2f17c95e315
lydiazgd <lydia.zhangge@gmail.com>
2016-04-15 10:34:12 -0600

    add project files

---
+++ src/tetris/JBrainTetris.java
@@ -0,0 +1,14 @@
+package tetris;
+
+import javax.swing.*;
+
+/**
+ * Created by Lydia on 2016/4/14.
+ */
+public class JBrainTetris {
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception ignored) { }
+    }
+}
